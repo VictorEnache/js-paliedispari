2228880c854a7c3380a4b1b370d7b024d1fd00b1
VictorEnache <victor22enache@gmail.com>
2022-04-01 16:00:01 +0200

    palidroma finito

---
+++ Palidroma/main.js
@@ -0,0 +1,29 @@
+/* Chiedere all’utente di inserire una parola. Creare una funzione 
+per capire se la parola inserita è palindroma */
+
+function parola_palidroma (parola){
+
+    let parola_al_contrario = '';
+    let parola_upperCase = parola.toUpperCase()
+    
+    for(let i = (parola.length)-1; i >= 0; i--){
+        let singolo_carattere = parola_upperCase[i]
+        parola_al_contrario += `${singolo_carattere}`
+    }
+    console.log(parola)
+    if(parola_upperCase === parola_al_contrario ){
+        return true
+    }
+    else{
+        return false
+    }
+
+
+}
+
+
+let parola_inserita = prompt('inserisci una parola e vediamo se è un palidroma').toLocaleUpperCase(); 
+
+
+//stampo a console il risultato della funzione.
+console.log(parola_palidroma(parola_inserita))
